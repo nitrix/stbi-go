d3028848fa75c102efb89f22a0c7208267cba058
Alex Belanger <nitrix@nitrix.me>
2024-10-30 20:16:38 -0400

    Updated stb + O3.

--- generator/generator.go
+++ generator/generator.go
@@ -7,32 +7,27 @@ import (
 )
 
 func main() {
-	err := copyFile("thirdparty/stb/stb_image.h", "stb_image.h")
-	if err != nil {
-		panic(err)
-	}
+	copyFile("thirdparty/stb/stb_image.h", "stb_image.h")
 }
 
-func copyFile(src, dst string) error {
+func copyFile(src, dst string) {
 	_ = os.MkdirAll(filepath.Dir(dst), 0750)
 	srcFile, err := os.Open(src)
 	if err != nil {
-		return err
+		panic(err)
 	}
 	defer srcFile.Close()
 
 	dstFile, err := os.Create(dst)
 	if err != nil {
-		return err
+		panic(err)
 	}
 	defer dstFile.Close()
 
 	_, err = srcFile.WriteTo(dstFile)
 	if err != nil {
-		return err
+		panic(err)
 	}
 
 	fmt.Printf("Copied file %s to %s\n", src, dst)
-
-	return nil
 }
